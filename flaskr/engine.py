a8018477bcf995f59829877d28454373aad1ce12
ayoubkachkach <ayoub.kachkach@gmail.com>
2018-11-25 14:17:48 +0100

    Fix various compilation errors. Code functional

--- flaskr/engine.py
+++ flaskr/engine.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-MATCH_LEN = 6 #in pixels
+MATCH_LEN = 1 #in pixels
 LINE_SPACING = 2*MATCH_LEN #spacing between consecutive lines
 DIM_SPACE = 240
 
@@ -21,19 +21,19 @@ class Match:
         self.angle = angle
         
         #projection of center to side of the match w.r.t. x and y axes resp.
-        self.hor_projection = center*math.cos(angle)
-        self.ver_projection = center*math.sin(angle)
+        self.hor_projection = (MATCH_LEN/2)*math.cos(angle)
+        self.ver_projection = (MATCH_LEN/2)*math.sin(angle)
         
         #upper_point --> point with higher x-coordinate
-        self.upper_point = Point(center + hor_project, center + ver_projection)
-        self.lower_point = Point(center - hor_project, center - ver_projection) 
+        self.upper_point = Point(self.center + self.hor_projection,self.center + self.ver_projection)
+        self.lower_point = Point(self.center - self.hor_projection, self.center - self.ver_projection) 
     
-        self.touches = touches_line()
+        self.touches = self.touches_line()
 
     def get_points(self):
         return (self.lower_point, self.upper_point)
 
-    def touches_line():
+    def touches_line(self):
         center_dist = self.center % LINE_SPACING
         
         return center_dist - self.ver_projection < 0 \
@@ -50,16 +50,17 @@ class Experiment:
         self.num_matches = num_matches
         self.matches = []
         self.touching_matches = 0 #no matches touching line initially
-    
+
         self.throw_matches(num_matches)
 
     def throw_matches(self, num_matches):
         centers = np.random.uniform(0, DIM_SPACE - MATCH_LEN/2, num_matches)
         angles = np.random.uniform(0, math.pi, num_matches)
         self.matches = [Match(center, angle) for center, angle in zip(centers, angles)]
-        self.touching_matches = sum(1 for match in matches if match.touches)
+        self.touching_matches = sum(1 for match in self.matches if match.touches)
     
 
 if(__name__ == "__main__"):
-    exp = Experiment(100)
+    num_matches = 90000
+    print(sum([num_matches/Experiment(num_matches).touching_matches for i in range(10)])/10)
 
